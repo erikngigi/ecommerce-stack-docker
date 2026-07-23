028e535a7b5d89cbcae38270cabb8cc4d4d75ec3
erikngigi <ericmosesngigi@gmail.com>
2026-07-23 14:25:28 +0300

    chore: update akeneo install script with permissions

--- templates/scripts/akeneo.sh
+++ templates/scripts/akeneo.sh
@@ -37,6 +37,7 @@ akeneo_exec bash -c "
     sed -i 's/^APP_DATABASE_USER=.*/APP_DATABASE_USER=$MYSQL_AKENEO_USER/' .env.local;
     sed -i 's/^APP_DATABASE_PASSWORD=.*/APP_DATABASE_PASSWORD=$MYSQL_AKENEO_PASSWORD/' .env.local;
     sed -i 's/^APP_INDEX_HOSTS=.*/APP_INDEX_HOSTS=$APP_INDEX_HOSTS/' .env.local;
+    sed -i 's/^AKENEO_PIM_URL=.*/AKENEO_PIM_URL=$AKENEO_PIM_URL/' .env.local;
 "
 
 # Update browserlist
@@ -45,6 +46,7 @@ akeneo_exec bash -c "cd $AKENEO_INST_DIR && yes | npx update-browserslist-db@lat
 
 # Install akeneo
 print_blue "Installing Akeneo"
+akeneo_exec bash -c "cd $AKENEO_INST_DIR && chmod -R 777 /var/cache var/logs public/media"
 akeneo_exec bash -c "cd $AKENEO_INST_DIR && NO_DOCKER=true make dev"
 
 print_green "Creating User $AKENEO_USERNAME"
