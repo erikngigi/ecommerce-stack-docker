bffb32525f8622b0138fadbcb5aa6ae05664b13d
erikngigi <ericmosesngigi@gmail.com>
2026-07-23 14:32:00 +0300

    chore: update akeneo script

--- templates/scripts/akeneo.sh
+++ templates/scripts/akeneo.sh
@@ -32,12 +32,12 @@ akeneo_exec bash -c "cd $AKENEO_INST_DIR && chmod u+x bin/console"
 print_green "Appending details to the Akeneo .env.local file"
 akeneo_exec bash -c "
     cd $AKENEO_INST_DIR &&
-    sed -i 's/^APP_DATABASE_HOST=.*/APP_DATABASE_HOST=$MYSQL_AKENEO_DATABASE_HOST/' .env.local;
-    sed -i 's/^APP_DATABASE_NAME=.*/APP_DATABASE_NAME=$MYSQL_AKENEO_DATABASE/' .env.local;
-    sed -i 's/^APP_DATABASE_USER=.*/APP_DATABASE_USER=$MYSQL_AKENEO_USER/' .env.local;
-    sed -i 's/^APP_DATABASE_PASSWORD=.*/APP_DATABASE_PASSWORD=$MYSQL_AKENEO_PASSWORD/' .env.local;
-    sed -i 's/^APP_INDEX_HOSTS=.*/APP_INDEX_HOSTS=$APP_INDEX_HOSTS/' .env.local;
-    sed -i 's/^AKENEO_PIM_URL=.*/AKENEO_PIM_URL=$AKENEO_PIM_URL/' .env.local;
+    sed -i 's|^APP_DATABASE_HOST=.*|APP_DATABASE_HOST=$MYSQL_AKENEO_DATABASE_HOST|' .env.local;
+    sed -i 's|^APP_DATABASE_NAME=.*|APP_DATABASE_NAME=$MYSQL_AKENEO_DATABASE|' .env.local;
+    sed -i 's|^APP_DATABASE_USER=.*|APP_DATABASE_USER=$MYSQL_AKENEO_USER|' .env.local;
+    sed -i 's|^APP_DATABASE_PASSWORD=.*|APP_DATABASE_PASSWORD=$MYSQL_AKENEO_PASSWORD|' .env.local;
+    sed -i 's|^APP_INDEX_HOSTS=.*|APP_INDEX_HOSTS=$APP_INDEX_HOSTS|' .env.local;
+    sed -i 's|^AKENEO_PIM_URL=.*|AKENEO_PIM_URL=$AKENEO_PIM_URL|' .env.local;
 "
 
 # Update browserlist
@@ -50,4 +50,10 @@ akeneo_exec bash -c "cd $AKENEO_INST_DIR && chmod -R 777 /var/cache var/logs pub
 akeneo_exec bash -c "cd $AKENEO_INST_DIR && NO_DOCKER=true make dev"
 
 print_green "Creating User $AKENEO_USERNAME"
-akeneo_exec bash -c "cd $AKENEO_INST_DIR && bin/console pim:user:create $AKENEO_USERNAME $AKENEO_PASSWORD $AKENEO_USER_EMAIL $AKENEO_FIRST_NAME $AKENEO_LAST_NAME $AKENEO_LOCALE --admin -n --env=dev"
+akeneo_exec bash -c "cd $AKENEO_INST_DIR && bin/console pim:user:create \
+  $AKENEO_USERNAME \
+  $AKENEO_PASSWORD \
+  $AKENEO_USER_EMAIL \
+  $AKENEO_FIRST_NAME \
+  $AKENEO_LAST_NAME \
+  $AKENEO_LOCALE --admin -n --env=dev"
